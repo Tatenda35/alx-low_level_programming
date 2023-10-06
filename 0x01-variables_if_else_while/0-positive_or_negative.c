32ccce93253659298fcdd784fb73489dee4306e0
Tatenda35 <tatndor@gmail.com>
2023-10-06 06:16:33 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
 /* 
  * main - main block executed
  * Description- an algorithm that assigns random variable n
