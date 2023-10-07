3d68eeb9be51507da271a4fb6b7952b718488512
Tatenda35 <tatndor@gmail.com>
2023-10-07 11:23:29 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,11 +13,9 @@
 int main(void)
 {
 	char alphabet = 'a';
-	char e;
-	char q;
 
 	for (alphabet = 'a'; alphabet <= 'z'; ++alphabet)
-		if (alphabet != 'e' || != 'q')
+		if (alphabet != 'e' && alphabet != 'q')
 		{
 			putchar(alphabet);
 		}
