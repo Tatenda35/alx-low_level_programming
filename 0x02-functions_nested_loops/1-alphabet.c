13fdb05184626143b338028f3fb52d7ff406ccf7
Tatenda35 <tatndor@gmail.com>
2023-10-10 00:27:04 -0700

    0x02. C - Functions, nested loops
    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * print_alphabet - print_alphabet function
+ *
+ * Description: An algotithim that prints the alphabet followed by a new line.
+ *
+ * Return: No Return type.
+ */
+void print_alphabet(void);
+{
+	char lowercase_alphab;
+
+	for (lowercase_alphab = 'a'; lowercase_alphab <= 'z'; lowercase_alphab++)
+	{
+		putchar(lowercase_alphab);
+	}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 # 0x02.C- Functions,nested loops tasks
  *0. _putchar,Write a program that prints _putchar, followed by a new line.
+1-alphabet.c; Write a function thatprints the alphabet, in lowercase, followed by a new line.
