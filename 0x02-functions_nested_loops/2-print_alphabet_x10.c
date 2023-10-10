203c5715df96d5bb4472332a111751998aa18729
Tatenda35 <tatndor@gmail.com>
2023-10-10 00:50:22 -0700

    0x02. C - Function, nested loops
    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <main.h>
+/**
+ * print_alphabet_x10 - print_alphabet_x10 function
+ *
+ * Description: An algorithm that writes a function
+ * that prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+		char lowercase_alphab;
+
+		while (lowercase_alphab <= 'z')
+		{
+			putchar(lowercase_alphab);
+			lowercase_alphab++;
+		}
+
+		purchar('\n');
+		count++;
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 # 0x02.C- Functions,nested loops tasks
  *0. _putchar,Write a program that prints _putchar, followed by a new line.
 1-alphabet.c; Write a function thatprints the alphabet, in lowercase, followed by a new line.
+ *2-print_alphabet_x10.c; Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.
