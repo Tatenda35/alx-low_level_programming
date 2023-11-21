ccb1208073f06e94b9a674f9bdd2263f6cfc1013
Tatenda35 <tatndor@gmail.com>
2023-11-20 23:20:57 -0800

    a

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -8,13 +11,14 @@
  *
  * Description: singly linked list node structure
  * for Holberton project
- */
+ **/
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
+int _putchar(char);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -24,19 +28,11 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
-
-/**
- * insert_nodeint_at_index - insert node at given index of linked list
- * @head: double header head
- * @index: where to insert node
- * @n: value to store in node
- * Return: new node or null if failed
- */
-listint_t *insert_nodeint_at_index(listint_t **head,
-				   unsigned int index, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
 #endif
